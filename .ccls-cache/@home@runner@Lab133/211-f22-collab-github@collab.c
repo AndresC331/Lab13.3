217b5362e8cfd0e87aab07566c84f0ea30a507f6
AndresCifuentes <>
2022-11-15 15:29:51 +0000

    Ad new function

---
+++ .ccls-cache/@home@runner@Lab133/211-f22-collab-github@collab.c
@@ -0,0 +1,38 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+void reed_ekidan2() {
+  printf("Dale Reed and Ellen Kidane\n");
+  printf("We learned that git and GitHub are amazing, powerful tools!\n");
+}
+
+void avarga53_acifu2() {
+  printf("Astrid Vargas and Andres Cifuentes\n");
+  printf("We learned that git and GitHub is an useful enviroment where we are able to share code and work with others, sepecifaclly when we are working in the same code but in different parts of it. One think that we struggle with was the pushing the code and making sure that we where commenting it in an ideal way so that other can see what we did.\n");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    reed_ekidan2();
+    avarga53_acifu2()
+
+  
+    return 0;
+}
